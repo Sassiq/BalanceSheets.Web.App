8b14286c5addd4525280f8f034bbbae6807023c8
stanislav.chekhovich@gmail.com <stanislav.chekhovich@gmail.com>
2022-10-28 18:49:24 +0300

    Update views. Remove some logic to FinancialClassViewModel.

--- BalanceSheetsApp.Web/Models/FinancialClassViewModel.cs
+++ BalanceSheetsApp.Web/Models/FinancialClassViewModel.cs
@@ -3,6 +3,12 @@
     public class FinancialClassViewModel
     {
         public string Name { get; set; }
+        public decimal OpBalanceActSum { get => this.Accounts.Sum(a => a.OpeningBalance.Active); }
+        public decimal OpBalancePasSum { get => this.Accounts.Sum(a => a.OpeningBalance.Passive); }
+        public decimal DebitSum { get => this.Accounts.Sum(a => a.Debit); }
+        public decimal CreditSum { get => this.Accounts.Sum(a => a.Credit); }
+        public decimal ClBalanceActSum { get => this.Accounts.Sum(a => a.ClosingBalance.Active); }
+        public decimal ClBalancePasSum { get => this.Accounts.Sum(a => a.ClosingBalance.Passive); }
         public ICollection<AccountViewModel> Accounts { get; set; }
     }
 }
